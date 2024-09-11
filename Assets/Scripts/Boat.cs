d50983901f53c33110d630091543a911eea7dbba
Etskyy <loxton.aiden@gmail.com>
2024-09-11 12:36:30 +1000

    Boat (Net)

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Boat : MonoBehaviour
+{
+    private Vector3 TargetSpot;
+    private Vector3 NetSpot;
+    public GameObject Net;
+    public float BoatSpeed;
+    public float NetSpeed;
+    private bool positionreached;
+    private bool netpull;
+    public float NetTime;
+    public float HoldTime;
+    public float HoldPlayerAbove;
+    public GameObject Player;
+    GameObject newNet;
+    void Start(){
+        float newz = Mathf.Abs(Random.Range(Player.gameObject.transform.position.z-20,gameObject.transform.position.z-20));
+        TargetSpot = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, newz);
+        NetSpot = new Vector3(gameObject.transform.position.x, Random.Range(-5f, 3f), gameObject.transform.position.z);
+        newNet = Instantiate(Net, NetSpot, Net.transform.rotation);
+        newNet.transform.parent = gameObject.transform;
+    }
+    void Update(){
+        if (!positionreached){
+            transform.position = Vector3.MoveTowards(transform.position, TargetSpot, (BoatSpeed * Time.deltaTime));
+            if (Vector3.Distance(transform.position, TargetSpot) < 0.001f){
+                StartCoroutine(NetCheck());
+                positionreached = true;
+            }
+        }
+        if (netpull){
+            newNet.transform.position = Vector3.MoveTowards(newNet.transform.position, transform.position, (NetSpeed * Time.deltaTime));
+            if (Vector3.Distance(transform.position, transform.position) < 0.001f){
+                netpull = false;
+                StartCoroutine(HoldPlayer());
+            }
+        }
+    }
+
+    IEnumerator NetCheck(){
+        //hold player here
+        yield return new WaitForSeconds(NetTime);
+        netpull = true;
+        //move player up
+    }
+
+    IEnumerator HoldPlayer(){
+        yield return new WaitForSeconds(HoldTime);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BoatSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class BoatSpawner : MonoBehaviour
+{
+    public GameObject Boat;
+
+    public float SpawnInterval;
+    public float minSpawnTimer = 10;
+    public float maxSpawnTimer = 20;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTimer(SpawnInterval));
+    }
+
+    IEnumerator SpawnTimer(float WaitTime){
+        yield return new WaitForSeconds(WaitTime);
+        SpawnBoat();
+        float spawnTime = Random.Range(minSpawnTimer, maxSpawnTimer);
+        StartCoroutine(SpawnTimer(spawnTime));
+    }
+
+    public void SpawnBoat(){
+        GameObject newBoat = Instantiate(Boat, new Vector3(gameObject.transform.position.x, 8.0f, gameObject.transform.position.z), gameObject.transform.rotation);
+        newBoat.GetComponent<Boat>().Player = gameObject.transform.parent.gameObject;
+    }
+}
